612479957cb34bb1acdaaa93ed7010ea4ba6e99d
Giulliano Bueno <giubueno@gmail.com>
2017-02-11 13:53:10 +0100

    Activeadmin

--- church/config/routes.rb
+++ church/config/routes.rb
@@ -1,4 +1,7 @@
 Rails.application.routes.draw do
+  devise_for :users, ActiveAdmin::Devise.config
+  devise_for :admin_users, ActiveAdmin::Devise.config
+  ActiveAdmin.routes(self)
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   root to: "home#index"
 end
