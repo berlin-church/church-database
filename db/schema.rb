7ab05b62f0000524af451029b48db54c3c845830
Jonathan Wiens <jonathan.wiens@gmail.com>
2017-03-04 14:56:06 +0100

    Added updating and viewing of the address in members view (#14)
    * Added updating and viewing of the address in members view
    
    * Satisfy rubocop

--- db/schema.rb
+++ db/schema.rb
@@ -1,4 +1,3 @@
-# frozen_string_literal: true
 # This file is auto-generated from the current state of the database. Instead
 # of editing this file, please use the migrations feature of Active Record to
 # incrementally modify your database, and then regenerate this schema definition.
@@ -11,93 +10,95 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20_170_225_131_310) do
-  create_table 'active_admin_comments', force: :cascade do |t|
-    t.string   'namespace'
-    t.text     'body'
-    t.string   'resource_id',   null: false
-    t.string   'resource_type', null: false
-    t.string   'author_type'
-    t.integer  'author_id'
-    t.datetime 'created_at',    null: false
-    t.datetime 'updated_at',    null: false
-    t.index %w(author_type author_id), name: 'index_active_admin_comments_on_author_type_and_author_id'
-    t.index ['namespace'], name: 'index_active_admin_comments_on_namespace'
-    t.index %w(resource_type resource_id), name: 'index_active_admin_comments_on_resource_type_and_resource_id'
+ActiveRecord::Schema.define(version: 20170225131310) do
+
+  create_table "active_admin_comments", force: :cascade do |t|
+    t.string   "namespace"
+    t.text     "body"
+    t.string   "resource_id",   null: false
+    t.string   "resource_type", null: false
+    t.string   "author_type"
+    t.integer  "author_id"
+    t.datetime "created_at",    null: false
+    t.datetime "updated_at",    null: false
+    t.index ["author_type", "author_id"], name: "index_active_admin_comments_on_author_type_and_author_id"
+    t.index ["namespace"], name: "index_active_admin_comments_on_namespace"
+    t.index ["resource_type", "resource_id"], name: "index_active_admin_comments_on_resource_type_and_resource_id"
   end
 
-  create_table 'addresses', force: :cascade do |t|
-    t.string   'street'
-    t.string   'street_number'
-    t.string   'area_code'
-    t.string   'city'
-    t.string   'country'
-    t.datetime 'created_at',    null: false
-    t.datetime 'updated_at',    null: false
-    t.integer  'member_id'
-    t.index ['member_id'], name: 'index_addresses_on_member_id'
+  create_table "addresses", force: :cascade do |t|
+    t.string   "street"
+    t.string   "street_number"
+    t.string   "area_code"
+    t.string   "city"
+    t.string   "country"
+    t.datetime "created_at",    null: false
+    t.datetime "updated_at",    null: false
+    t.integer  "member_id"
+    t.index ["member_id"], name: "index_addresses_on_member_id"
   end
 
-  create_table 'admin_users', force: :cascade do |t|
-    t.string   'email',                  default: '', null: false
-    t.string   'encrypted_password',     default: '', null: false
-    t.string   'reset_password_token'
-    t.datetime 'reset_password_sent_at'
-    t.datetime 'remember_created_at'
-    t.integer  'sign_in_count', default: 0, null: false
-    t.datetime 'current_sign_in_at'
-    t.datetime 'last_sign_in_at'
-    t.string   'current_sign_in_ip'
-    t.string   'last_sign_in_ip'
-    t.datetime 'created_at',                          null: false
-    t.datetime 'updated_at',                          null: false
-    t.index ['email'], name: 'index_admin_users_on_email', unique: true
-    t.index ['reset_password_token'], name: 'index_admin_users_on_reset_password_token', unique: true
+  create_table "admin_users", force: :cascade do |t|
+    t.string   "email",                  default: "", null: false
+    t.string   "encrypted_password",     default: "", null: false
+    t.string   "reset_password_token"
+    t.datetime "reset_password_sent_at"
+    t.datetime "remember_created_at"
+    t.integer  "sign_in_count",          default: 0,  null: false
+    t.datetime "current_sign_in_at"
+    t.datetime "last_sign_in_at"
+    t.string   "current_sign_in_ip"
+    t.string   "last_sign_in_ip"
+    t.datetime "created_at",                          null: false
+    t.datetime "updated_at",                          null: false
+    t.index ["email"], name: "index_admin_users_on_email", unique: true
+    t.index ["reset_password_token"], name: "index_admin_users_on_reset_password_token", unique: true
   end
 
-  create_table 'families', force: :cascade do |t|
-    t.string   'name'
-    t.datetime 'created_at', null: false
-    t.datetime 'updated_at', null: false
+  create_table "families", force: :cascade do |t|
+    t.string   "name"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
   end
 
-  create_table 'members', force: :cascade do |t|
-    t.string   'first_name'
-    t.string   'last_name'
-    t.string   'gender'
-    t.date     'birthday'
-    t.string   'email'
-    t.string   'phone1'
-    t.string   'phone2'
-    t.datetime 'created_at', null: false
-    t.datetime 'updated_at', null: false
-    t.integer  'family_id'
-    t.integer  'address_id'
-    t.index ['address_id'], name: 'index_members_on_address_id'
-    t.index ['family_id'], name: 'index_members_on_family_id'
+  create_table "members", force: :cascade do |t|
+    t.string   "first_name"
+    t.string   "last_name"
+    t.string   "gender"
+    t.date     "birthday"
+    t.string   "email"
+    t.string   "phone1"
+    t.string   "phone2"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.integer  "family_id"
+    t.integer  "address_id"
+    t.index ["address_id"], name: "index_members_on_address_id"
+    t.index ["family_id"], name: "index_members_on_family_id"
   end
 
-  create_table 'users', force: :cascade do |t|
-    t.string   'email',                  default: '', null: false
-    t.string   'encrypted_password',     default: '', null: false
-    t.string   'reset_password_token'
-    t.datetime 'reset_password_sent_at'
-    t.datetime 'remember_created_at'
-    t.integer  'sign_in_count', default: 0, null: false
-    t.datetime 'current_sign_in_at'
-    t.datetime 'last_sign_in_at'
-    t.string   'current_sign_in_ip'
-    t.string   'last_sign_in_ip'
-    t.datetime 'created_at',                          null: false
-    t.datetime 'updated_at',                          null: false
-    t.string   'first_name'
-    t.string   'last_name'
-    t.string   'gender'
-    t.date     'birthday'
-    t.string   'phone1'
-    t.string   'phone2'
-    t.date     'joined_at'
-    t.index ['email'], name: 'index_users_on_email', unique: true
-    t.index ['reset_password_token'], name: 'index_users_on_reset_password_token', unique: true
+  create_table "users", force: :cascade do |t|
+    t.string   "email",                  default: "", null: false
+    t.string   "encrypted_password",     default: "", null: false
+    t.string   "reset_password_token"
+    t.datetime "reset_password_sent_at"
+    t.datetime "remember_created_at"
+    t.integer  "sign_in_count",          default: 0,  null: false
+    t.datetime "current_sign_in_at"
+    t.datetime "last_sign_in_at"
+    t.string   "current_sign_in_ip"
+    t.string   "last_sign_in_ip"
+    t.datetime "created_at",                          null: false
+    t.datetime "updated_at",                          null: false
+    t.string   "first_name"
+    t.string   "last_name"
+    t.string   "gender"
+    t.date     "birthday"
+    t.string   "phone1"
+    t.string   "phone2"
+    t.date     "joined_at"
+    t.index ["email"], name: "index_users_on_email", unique: true
+    t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
+
 end
