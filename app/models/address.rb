373b1a461f8aeafba196bd24603cdef3f0b12122
Jonathan Wiens <jonathan.wiens@gmail.com>
2017-03-04 12:42:11 +0100

    Added some validation and prettified things

--- app/models/address.rb
+++ app/models/address.rb
@@ -1,4 +1,6 @@
 # frozen_string_literal: true
 class Address < ApplicationRecord
   belongs_to :member
+
+  #validates :city, with: /^[A-Za-z]+$/
 end
--- app/models/member.rb
+++ app/models/member.rb
@@ -3,7 +3,9 @@ class Member < ApplicationRecord
   belongs_to :family
   has_one :address
   accepts_nested_attributes_for :address
+  validates_format_of :first_name, :last_name, :with => /\A[a-z]+\z/i
   validates :first_name, presence: true
   validates :last_name, presence: true
   validates :gender, presence: true
+  validates :email, format: { with: /\A([^@\s]+)@((?:[-a-z0-9]+\.)+[a-z]{2,})\z/i }
 end
