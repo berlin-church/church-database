e119f0841cabbf4441fff59d4ff8e7a361562c9f
Jonathan Wiens <jonathan.wiens@gmail.com>
2017-03-11 11:18:21 +0100

    Delete users and cleanup migration files (#18)
    * Ignoring all of db files
    
    * Remove all user references
    
    * Cleanup migration files

--- db/migrate/20170225111932_add_family_reference_to_members.rb
+++
@@ -1,6 +0,0 @@
-# frozen_string_literal: true
-class AddFamilyReferenceToMembers < ActiveRecord::Migration[5.0]
-  def change
-    add_reference :members, :family, foreign_key: true
-  end
-end
--- db/migrate/20170225122418_add_address_to_members.rb
+++
@@ -1,6 +0,0 @@
-# frozen_string_literal: true
-class AddAddressToMembers < ActiveRecord::Migration[5.0]
-  def change
-    add_reference :members, :address, foreign_key: true
-  end
-end
