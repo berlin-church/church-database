0807800519b8a30c06e43a31c0079cc9a80741ee
Jonathan Wiens <jonathan.wiens@gmail.com>
2017-03-11 12:10:10 +0100

    Adjusted seed to AdminUser instead of User

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,2 +1,2 @@
 # frozen_string_literal: true
-User.create!(email: 'admin@example.com', password: 'password', password_confirmation: 'password')
+AdminUser.create!(email: 'admin@example.com', password: 'password', password_confirmation: 'password')
