4174e791a4767d49c29bf2e71ef8150ab373f444
Jonathan Wiens <jonathan.wiens@gmail.com>
2017-03-11 10:55:37 +0100

    Satisfy rubocop

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,6 @@
 # frozen_string_literal: true
 Rails.application.routes.draw do
-#devise_for :users, ActiveAdmin::Devise.config
+  # devise_for :users, ActiveAdmin::Devise.config
   devise_for :admin_users, ActiveAdmin::Devise.config
   ActiveAdmin.routes(self)
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
