6229676ee81208f0acf63465cd3622b74a64124c
Giulliano Bueno <giubueno@gmail.com>
2017-05-31 23:44:06 +0200

    Lint fix

--- config/initializers/raven.rb
+++ config/initializers/raven.rb
@@ -1,6 +1,7 @@
+# frozen_string_literal: true
 Raven.configure do |config|
-  if  Rails.env.production?
-    config.dsn = ENV["SENTRY"]
+  if Rails.env.production?
+    config.dsn = ENV['SENTRY']
     config.environments = ['production']
     config.sanitize_fields = Rails.application.config.filter_parameters.map(&:to_s)
   end
