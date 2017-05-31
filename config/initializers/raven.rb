63e96c9da45367a8e57fb7eff22f247140f17141
Giulliano Bueno <giubueno@gmail.com>
2017-05-31 23:41:37 +0200

    Config Raven only if it is in production

---
+++ config/initializers/raven.rb
@@ -0,0 +1,7 @@
+Raven.configure do |config|
+  if  Rails.env.production?
+    config.dsn = ENV["SENTRY"]
+    config.environments = ['production']
+    config.sanitize_fields = Rails.application.config.filter_parameters.map(&:to_s)
+  end
+end
