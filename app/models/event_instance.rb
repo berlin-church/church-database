d17c20d9c5fb412bc74f7402498e218cec2868ee
Giulliano Bueno <giubueno@gmail.com>
2018-01-01 19:01:59 +0100

    Use the admin dashboard as root path

--- app/models/event_instance.rb
+++ app/models/event_instance.rb
@@ -2,8 +2,10 @@
 class EventInstance < ApplicationRecord
   belongs_to :address
   belongs_to :event
+  has_many :attendees
+  has_many :members, through: :attendees
 
   def name
-    "#{event.name} #{self.start_time.month}/#{self.start_time.year}"
+    "#{event&.name} #{self.start_time.month}/#{self.start_time.year}"
   end
 end
