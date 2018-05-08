827d42895254b76a06ccb8ee6d0347328a317bf7
Jonathan Wiens <jonathan.wiens@gmail.com>
2018-05-08 20:39:57 +0200

    Delete duplicate admin_users.rb file

--- app/admin/admin_user.rb
+++ app/admin/admin_user.rb
@@ -17,10 +17,12 @@ ActiveAdmin.register AdminUser do
   index do
     selectable_column
     id_column
+    column :first_name
+    column :last_name
     column :email
     # column :current_sign_in_at
-    # column :sign_in_count
-    column :created_at
+    column :sign_in_count
+    # column :created_at
     actions
   end
 
