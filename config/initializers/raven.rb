86c008b50a9849a358ea95e50539ddfc2462d0a3
Giulliano Bueno <giubueno@gmail.com>
2018-09-21 18:26:06 +0200

    Update the documentation to document the environment variables used. (#96)

--- config/initializers/raven.rb
+++ config/initializers/raven.rb
@@ -1,18 +1,13 @@
 # frozen_string_literal: true
-# Raven.configure do |config|
-#   if Rails.env.production?
-#     #config.dsn = ENV['SENTRY']
-#     config.environments = ['production']
-#     config.sanitize_fields = Rails.application.config.filter_parameters.map(&:to_s)
-#   end
-# end
-
-# Raven.configure do |config|
-#   logger = ::Raven::Logger.new(STDOUT)
-#   logger.level = "error"
-#   config.logger = logger
-# end
-
-Raven.configure do |config|
-  config.dsn = "foo" unless Rails.env.development?
+if ENV['SENTRY']
+  Raven.configure do |config|
+    if Rails.env.production?
+      config.dsn = ENV['SENTRY']
+      config.environments = ['production']
+      config.sanitize_fields = Rails.application.config.filter_parameters.map(&:to_s)
+    end
+    logger = ::Raven::Logger.new(STDOUT)
+    logger.level = "error"
+    config.logger = logger
+  end
 end
